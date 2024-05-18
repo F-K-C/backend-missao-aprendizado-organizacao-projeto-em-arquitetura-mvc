ce14ddfe7236783def8e81b89265b3e6011b5347
F-K-C <fakocarvalho@gmail.com>
2024-05-18 19:40:25 +0000

    Limpeza de comentário no Index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const { connectToDatabase } = require('./db/database-connection')
+
 const personagemRouter = require('./personagem/personagem.router')
-//const { MongoClient, ObjectId } = require('mongodb')
 
 async function main() {
 
     await connectToDatabase()
 
-
-    // const collection = db.collection('personagem')
     const app = express()
+
     app.use(express.json())
 
     app.get('/', function (req, res) {
